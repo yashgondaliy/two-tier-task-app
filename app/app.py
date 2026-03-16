a3a0ddc9b5ddbbdded09b698a02ea667e1f343c1
yashgondaliy <yashgondaliya0@gmail.com>
2026-03-16 04:46:04 +0000

    added app

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host=os.environ.get("DB_HOST"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
+    database=os.environ.get("DB_NAME")
+)
+
+cursor = db.cursor()
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        task = request.form["task"]
+        cursor.execute("INSERT INTO tasks (task) VALUES (%s)", (task,))
+        db.commit()
+
+    cursor.execute("SELECT * FROM tasks")
+    tasks = cursor.fetchall()
+
+    return render_template("index.html", tasks=tasks)
+
+app.run(host="0.0.0.0", port=5000)
